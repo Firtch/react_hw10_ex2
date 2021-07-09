e1432129bc58ccec0123edec855d9522ee91b41b
Firtch <firtch@gmail.com>
2021-07-09 12:01:20 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
+import data from './data.js';
+import Posts from './Posts.js';
+import Post from './Post.js';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  // HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">My Posts</h2>
+        </header>
+        <section>
+          <Switch>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+            <Route path="/">
+              <Posts posts={data} />
+            </Route>         
+            <Redirect from="/user" to="/login" />               
+          </Switch>                    
+        </section>
+      </div>
+    </Router>
   );
 }
 
